8bf96affcc73286f0d8c7be368963890fc3fe49f
randi219 <diran@email.arizona.edu>
2016-10-23 22:40:12 -0700

    modified

--- example20161020.jl
+++ example20161020.jl
@@ -148,18 +148,19 @@ for i in 1:nsnp
                               zeros(3), sim_dat[1], sim_dat[3])
 
     result[i,9] = nw3[1][2]
-    result[i,10] = nw2[2][2]
-    result[i,11] = nw2[6][2]
-    result[i,12] = nw2[7][2]
+    result[i,10] = nw3[2][2]
+    result[i,11] = nw3[6][2]
+    result[i,12] = nw3[7][2]
 end
 
+sum(result[:,12] .< 0.05)/nsnp
 sum(result[:,4] .< 0.05)/nsnp
 sum(result[:,8] .< 0.05)/nsnp
-sum(result[:,12] .< 0.05)/nsnp
 
 
 
-bb[:,2] = 0.1
+
+bb[:,2] = 0.2
 
 result = zeros(nsnp, 12)
 
@@ -185,11 +186,101 @@ for i in 1:nsnp
                               zeros(3), sim_dat[1], sim_dat[3])
 
     result[i,9] = nw3[1][2]
-    result[i,10] = nw2[2][2]
-    result[i,11] = nw2[6][2]
-    result[i,12] = nw2[7][2]
+    result[i,10] = nw3[2][2]
+    result[i,11] = nw3[6][2]
+    result[i,12] = nw3[7][2]
 end
 
+sum(result[:,12] .< 0.05)/nsnp
 sum(result[:,4] .< 0.05)/nsnp
 sum(result[:,8] .< 0.05)/nsnp
+
+
+
+
+# beta not equal - different beta1
+
+# type I error
+bb = [2.0 0.0 -1.0;
+      2.0 0.5 -1.0;
+      2.0 0.0 -1.0;
+      2.0 0.0 -1.0;
+      2.0 0.0 -1.0]
+
+result = zeros(nsnp, 12)
+
+# with/without center indicator
+for i in 1:nsnp
+    sim_dat = sim_logistic(ncenter, nsample, bb, 0.7, ['b',]; strata=true, seed=1234)
+    nw2 = newton_distributed2(logistic_loglik, logistic_gradient, logistic_hessian,
+                              zeros(7), sim_dat[1], sim_dat[2])
+    result[i,1] = nw2[1][2]
+    result[i,2] = nw2[2][2]
+    result[i,3] = nw2[6][2]
+    result[i,4] = nw2[7][2]
+
+    logstat = logistic_stat(sim_dat[1], sim_dat[2], 3, 2)
+    meta2 = meta_logistic(logstat[1],logstat[2],logstat[3], logstat[4])
+
+    result[i,5] = meta2[1]
+    result[i,6] = sqrt(meta2[2])
+    result[i,7] = meta2[3]
+    result[i,8] = meta2[4]
+
+    nw3 = newton_distributed2(logistic_loglik, logistic_gradient, logistic_hessian,
+                              zeros(3), sim_dat[1], sim_dat[3])
+
+    result[i,9] = nw3[1][2]
+    result[i,10] = nw3[2][2]
+    result[i,11] = nw3[6][2]
+    result[i,12] = nw3[7][2]
+end
+
 sum(result[:,12] .< 0.05)/nsnp
+sum(result[:,4] .< 0.05)/nsnp
+sum(result[:,8] .< 0.05)/nsnp
+
+
+# beta not equal - different covariates
+
+# type I error
+bb = [2.0 0.0 -1.0;
+      2.0 0.0  1.0;
+      2.0 0.0 -1.5;
+      2.0 0.0 -0.5;
+      2.0 0.0  0.5]
+
+bb[:,2] = 0.3
+
+result = zeros(nsnp, 12)
+
+# with/without center indicator
+for i in 1:nsnp
+    sim_dat = sim_logistic(ncenter, nsample, bb, 0.7, ['b',]; strata=true, seed=1234)
+    nw2 = newton_distributed2(logistic_loglik, logistic_gradient, logistic_hessian,
+                              zeros(7), sim_dat[1], sim_dat[2])
+    result[i,1] = nw2[1][2]
+    result[i,2] = nw2[2][2]
+    result[i,3] = nw2[6][2]
+    result[i,4] = nw2[7][2]
+
+    logstat = logistic_stat(sim_dat[1], sim_dat[2], 3, 2)
+    meta2 = meta_logistic(logstat[1],logstat[2],logstat[3], logstat[4])
+
+    result[i,5] = meta2[1]
+    result[i,6] = sqrt(meta2[2])
+    result[i,7] = meta2[3]
+    result[i,8] = meta2[4]
+
+    nw3 = newton_distributed2(logistic_loglik, logistic_gradient, logistic_hessian,
+                              zeros(3), sim_dat[1], sim_dat[3])
+
+    result[i,9] = nw3[1][2]
+    result[i,10] = nw3[2][2]
+    result[i,11] = nw3[6][2]
+    result[i,12] = nw3[7][2]
+end
+
+sum(result[:,12] .< 0.05)/nsnp
+sum(result[:,4] .< 0.05)/nsnp
+sum(result[:,8] .< 0.05)/nsnp
